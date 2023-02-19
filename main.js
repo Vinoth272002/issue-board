b800a54bbee82b991c5e539d1f48b18660b116b9
Vinoth272002 <121481723+Vinoth272002@users.noreply.github.com>
2023-02-19 14:10:37 +0530

    Add files via upload
    issue

---
+++ main.js
@@ -0,0 +1,6 @@
+
+let textValue = localStorage.getItem("textvalue");
+let textArea = localStorage.getItem("textarea");
+document.querySelector(".line1").innerHTML= textValue;
+document.querySelector(".line2").innerHTML= textArea;
+
